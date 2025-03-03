2366b57c38a0a0747b3af50ff20c08b0008d423d
pingusdelingus <alphaspammail2016@gmail.com>
2025-03-02 22:13:36 -0500

    added alerts to file

--- sketchybarBland/plugins/message.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-source "$HOME/.config/sketchybar/colors.sh" # Loads all defined colors
-
-sketchybar --set $NAME  label="$MSG" \
-                        icon="$ICON" \
-                        icon.color=$FGC \
-                        label.color=$FGC \
-                        background.color="$BGC"
